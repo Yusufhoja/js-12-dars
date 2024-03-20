0991625be382f62f1c15e1c064d72a7d93e08952
Yusufhoja <usufhozah@gmail.com>
2024-03-20 19:44:47 +0500

    js 12-dars

---
+++ script.js
@@ -0,0 +1,64 @@
+
+let day =+prompt("son kiriting");
+let kun;
+
+switch (day) {
+    case 1:
+        kun = 'Dushanba';
+        break;
+    case 2:
+        kun = 'Seshanba';
+        break;
+    case 3:
+        kun = 'Chorshanba';
+        break;
+    case 4:
+        kun = 'Payshanba';
+        break;
+    case 5:
+        kun = 'Juma';
+        break;
+    case 6:
+        kun = 'Shanba';
+        break;
+    case 7:
+        kun = 'Yakshanba';
+        break;
+    default:
+        kun = 'Notogri kiritilgan kun';
+}
+
+alert(kun); 
+
+
+// let a =prompt("soz kiriting")
+// if(a==="A"){
+//     alert("siz alochisiz")
+// }
+// else if(a==="B"){
+//     alert("siz o'rtacha o'qisiz")
+// }
+// else if (a==="C","F"){
+//     alert("siz kalidorda oqisiz")
+// }
+// else{
+//     alert("siz vapseh oqimisiz")
+// }
+
+// let harflar=prompt("A,B,C");
+// let b= harflar.toUpperCase();
+
+// switch (b) {
+//     case  "A": 
+//         alert("alochisiz")
+//         break;
+//     case "B": 
+//         alert("ortacha")
+//         break;
+//     case 'C': 
+//         alert("ikkichisiz")
+//         break;
+//     default:
+//         alert("siz maktabda oqimaysiz")
+//         break;
+// }
